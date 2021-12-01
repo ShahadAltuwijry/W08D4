1eaca8c735f17c8eb38fd08f754fed1d3d631269
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-01 23:04:31 +0300

    like controller updated

--- routers/controller/like.js
+++ routers/controller/like.js
@@ -3,18 +3,28 @@ const likeModel = require("./../../db/module/like");
 const newLike = (req, res) => {
   const { id } = req.params;
   try {
-    const newLike = new likeModel({
-      postId: id,
-      userId: req.addedToken.id,
-    });
-
-    newLike
-      .save()
-      .then((result) => {
-        res.status(201).send(result);
+    likeModel
+      .findOneAndDelete({
+        $and: [{ postId: id }, { userId: req.addedToken.id }],
       })
-      .catch((err) => {
-        res.status(404).send(err);
+      .then((result) => {
+        if (result) {
+          res.status(200).send("unliked successfuly");
+        } else {
+          const newLike = new likeModel({
+            postId: id,
+            userId: req.addedToken.id,
+          });
+
+          newLike
+            .save()
+            .then((result) => {
+              res.status(201).send(result);
+            })
+            .catch((err) => {
+              res.status(404).send(err);
+            });
+        }
       });
   } catch (error) {
     res.status(404).json(error.message);
