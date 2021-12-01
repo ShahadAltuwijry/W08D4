0dd1e30a3f5df0101891b253d7fd05b241b79d5d
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-01 15:18:19 +0300

    like updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const roleRouter = require("./routers/route/role");
 const userRouter = require("./routers/route/user");
 const postRouter = require("./routers/route/post");
 const commentRouter = require("./routers/route/comment");
+const likeRouter = require("./routers/route/like");
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(roleRouter);
 app.use(userRouter);
 app.use(postRouter);
 app.use(commentRouter);
+app.use(likeRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- routers/route/like.js
+++ routers/route/like.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const likeRouter = express.Router();
 
-const { like, getRoles } = require("./../controller/role");
+const { like } = require("./../controller/like");
 
 likeRouter.post("/like", like);
-likeRouter.get("/roles", getRoles);
+// likeRouter.get("/roles", getRoles);
 
 module.exports = likeRouter;
